895bce43ff21b5cf30fe6e06c6086a19b2c19f2e
Bikash Halder <124695308+bikashhalder109@users.noreply.github.com>
2023-04-14 21:02:36 +0530

    Add files via upload

---
+++ Bikash.py
@@ -0,0 +1,73 @@
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from telegraph import upload_file
+import os
+
+bgt=Client(
+    "Bikash",
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    bot_token = os.environ["BOT_TOKEN"]
+)
+
+@bgt.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    text = f"""
+Heya {message.from_user.mention},
+I am here to generate Telegraph links for your media files.
+
+Simply send a valid media file directly to this chat.
+Valid file types are 'jpeg', 'jpg', 'png', 'mp4' and 'gif'.
+
+To generate links in **group chats**, add me to your supergroup and send the command <code>/tl</code> as a reply to a valid media file.
+
+🏠 | [Home](https://t.me/BikashGadgetsTech)
+            """
+    await bgt.send_message(message.chat.id, text, disable_web_page_preview=True)
+    
+
+@bgt.on_message(filters.media & filters.private)
+async def get_link_private(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/private/"
+            local_path = await message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return                 
+    except Exception:
+        pass        
+
+@bgt.on_message(filters.command('tl'))
+async def get_link_group(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/group/"
+            local_path = await message.reply_to_message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return         
+    except Exception:
+        pass                                           
+
+print("Bot is alive!")
+bgt.run()
+
